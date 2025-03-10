d76713b8c2ce19c9e4e25c9614d8d42ccbc4ffad
Arati1008 <aratimadnaik@gmail.com>
2025-03-10 16:23:33 +0530

    final

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-resource "google_storage_bucket" "my_bucket" {
-  name          = var.bucket_name
-  region      = var.region
-  storage_class= var.storage_class
-
-  versioning{
-    enabled=true
-  }
-
-  lifecyle {
-    prevent_destroy = true
-  }
+module "bucket"{
+    source= "./storage"
 }
---
+++ storage/bucket.tf
@@ -0,0 +1,13 @@
+resource "google_storage_bucket" "my_bucket" {
+  name          = var.bucket_name
+  location      = var.location
+  storage_class= var.storage_class
+
+  versioning{
+    enabled=true
+  }
+
+  lifecyle {
+    prevent_destroy = true
+  }
+}
