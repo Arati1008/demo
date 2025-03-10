58a8429729b2be33278beaa5bd3f813f9a4becd6
Arati1008 <aratimadnaik@gmail.com>
2025-03-10 17:00:36 +0530

    final

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "bucket"{
     source= "./storage"
-    name          = var.bucket_name
+    bucket_name   = var.bucket_name
     location      = var.location
     storage_class= var.storage_class
 }
