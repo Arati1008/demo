26eff66fd0c0e39e15764841a79ca27d2275a878
Arati1008 <aratimadnaik@gmail.com>
2025-03-10 16:53:29 +0530

    final

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+bucket_name= "my-demo-bucket-name-128955"
+location= "US"
+storage_class= "STANDARD"
+region= "us-central1"
---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "region"{
+    type= string
+    default= "us-central1" 
+}
+
+variable "bucket_name"{
+    type = string
+    default = ""
+}
+
+variable "location"{
+    type = string
+    default= "us"
+}
+
+variable "storage_class"{
+    type = string
+    default= "STANDARD"
+}
+
