00184533635c76196bf8e3b0bdf03ca637f8cd2b
Arati1008 <aratimadnaik@gmail.com>
2025-03-12 13:16:20 +0530

    added new

---
+++ vm/main.tf
@@ -0,0 +1,36 @@
+resource "google_compute_instance" "vm_instance" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10-buster-v20201014"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    subnetwork = google_compute_subnetwork.subnet.self_link
+    access_config {
+      // Automatically create an external IP
+    }
+}
+
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    echo "Hello, World!" > /var/log/startup-script.log
+  EOT
+}
+
+# output "vm_instance" {
+#   value = google_compute_instance.vm.name
+# }
+# output "vpc_id" {
+#   value = google_compute_network.vpc.self_link
+# }
+
+# output "subnet_id" {
+#   value = google_compute_subnetwork.subnet.self_link
+# }
