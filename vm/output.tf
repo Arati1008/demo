b74a210c7139350572a42ef25f5c70aab7d852a8
Arati1008 <aratimadnaik@gmail.com>
2025-03-21 20:44:48 +0530

    commit to dev 1

--- vm/output.tf
+++ vm/output.tf
@@ -1,7 +1,7 @@
-output "vpc_id" {
-  value = google_compute_network.vpc_network.self_link
-}
+# output "vpc_id" {
+#   value = google_compute_network.vpc_network.self_link
+# }
 
-output "subnet_id" {
-  value = google_compute_subnetwork.subnet.self_link
-}
+# output "subnet_id" {
+#   value = google_compute_subnetwork.subnet.self_link
+# }
