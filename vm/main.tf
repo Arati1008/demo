ec99d0ae0933013fa557e89226f07b8181d51038
Arati1008 <aratimadnaik@gmail.com>
2025-10-29 17:19:29 +0530

    Update main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -1,4 +1,4 @@
-resource "google_compute_instance" "vm_instance" {
+resource "google_compute_instance" "vm_instance1" {
   name         = var.vm_name
   machine_type = var.machine_type
   zone         = var.zone
